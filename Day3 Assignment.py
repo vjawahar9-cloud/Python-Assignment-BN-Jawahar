4ed7f4c7b241e6576c0e55e0610bdbaad58c5b74
vjawahar9-cloud <vjawahar9@gmail.com>
2026-02-24 23:23:51 +0530

    hii

---
+++ Day3 Assignment.py
@@ -0,0 +1,21 @@
+#Question 1
+
+level1= int(input("Enter level one score"))
+
+if level1>7:
+    print("level1 round is clear")
+
+
+    level2=int(input("Enter level two score"))
+
+    if level2>8:
+        
+        print("you are selected")
+
+    else:
+        print("rejected in level two")
+else:
+     print("rejected in level one")
+
+
+        

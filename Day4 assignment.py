d390a6ba87e5f7318bde84dfb5fcedfa46693ca5
vjawahar9-cloud <vjawahar9@gmail.com>
2026-02-27 17:34:36 +0530

    hii

---
+++ Day4 assignment.py
@@ -0,0 +1,78 @@
+
+#Question 1
+
+
+'''number=int(input("Enter a number"))
+
+count=1
+
+while count<=10:
+
+    print(number,"x",count, "=" ,number*count)
+    count+=1'''
+
+    
+'''output =
+
+        5 x 1 = 5
+        5 x 2 = 10
+        5 x 3 = 15
+        5 x 4 = 20
+        5 x 5 = 25
+        5 x 6 = 30
+        5 x 7 = 35
+        5 x 8 = 40
+        5 x 9 = 45
+        5 x 10 = 50'''
+
+
+
+#Question 2
+
+'''for num in range(1,21):
+    if num%3==0:
+        continue
+    print(num ,end = " ")'''
+
+'''output =
+
+          1 2 4 5 7 8 10 11 13 14 16 17 19 20 '''
+
+
+
+
+#Question 3
+
+'''for i in range(1, 6):
+    for j in range(1,i + 1):
+        print(j, end=" ")
+    print()'''
+
+
+'''output = 1 
+            1 2 
+            1 2 3 
+            1 2 3 4 
+            1 2 3 4 5 '''
+
+
+
+
+
+#Question4
+
+for i in range(5,0,-1):
+    for j in range(1,i+1):
+        print(j,end="")
+        
+    print()
+    
+
+
+
+
+
+
+
+
+    

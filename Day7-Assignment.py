56d92328d4648940c405f14876e78feb774bfd7a
vjawahar9-cloud <vjawahar9@gmail.com>
2026-03-02 23:05:29 +0530

    hii

---
+++ Day7-Assignment.py
@@ -0,0 +1,30 @@
+#Analyze and samples of tuple
+
+#1.Ordered:
+
+elements=[10,20,30,40,50,60,70]
+print("elements",elements)
+                               #output:[10,20,30,40,50,60,70]
+
+
+#2.Immutable:
+
+'''elements=(1,2,3)
+elements[0]=100'''
+                              #output= Error
+
+
+#3.Allow Duplicates
+
+elements=[10,20,30,10]
+print(elements)
+                              #output=[10,20,30,10]
+
+
+
+#4.It stores different data types
+
+elements=(10,20,"jawahar",11.9,"true")
+print(elements)
+
+

f52b955d69f988198ed07ba5954879c2cf2e861a
vjawahar9-cloud <vjawahar9@gmail.com>
2026-03-13 18:53:34 +0530

    hii

--- Day14-Assignment.py
+++ Day14-Assignment.py
@@ -1,135 +1,135 @@
 
 
-#1.IF ELSE PROGRAM
+1.IF ELSE PROGRAM
 
-#student grade:
+student grade:
 
-##mark=int(input("Enter student mark"))
-##
-##if mark>=90:
-##    print("Grade A")
-##elif mark>=75:
-##    print("Grade B")
-##elif mark>=35:
-##    print("Grade C")
-##else:
-##    print("fail")
+mark=int(input("Enter student mark"))
 
-
+if mark>=90:
+    print("Grade A")
+elif mark>=75:
+    print("Grade B")
+elif mark>=35:
+    print("Grade C")
+else:
+    print("fail")
 
 
 
-#2. WHILE PROGRAM
+
+
+2. WHILE PROGRAM
 
     #print numbers 1 to 10:
 
-##i=1
-##while i <= 10:
-##    print(i)
-##    i=i+1
+i=1
+while i <= 10:
+    print(i)
+    i=i+1
 
 
     #print number 10 to 1:
-##i=10
-##while i >=1:
-##    print(i)
-##    i=i-1
-##    
+i=10
+while i >=1:
+    print(i)
+    i=i-1
+    
 
 
     #print 1 to 10 and remove number 3:
-##i=1
-##while i <=10:
-##    if i==3:
-##        i=i+1
-##        continue
-##    print(i)
-##    i=i+1
+i=1
+while i <=10:
+    if i==3:
+        i=i+1
+        continue
+    print(i)
+    i=i+1
 
 
 
 
 
 
-#3.FOR LOOP PROGRAM
+3.FOR LOOP PROGRAM
 
       #Print list elements
 
-##numbers = [10,20,30,40,50]
-##
-##for n in numbers:
-##    print(n)
+numbers = [10,20,30,40,50]
+
+for n in numbers:
+    print(n)
 
 
 
         #print students marks
-##
-##marks=[95,75,90,65,85]
-##
-##for i in marks:
-##    print(i)
+
+marks=[95,75,90,65,85]
+
+for i in marks:
+    print(i)
 
 
 
 
 
-#4. CLASS AND OBJECT PROGRAM
+4. CLASS AND OBJECT PROGRAM
 
 
-##class student:
-##    def __init__(self,studentname,studentmark):
-##        self.studentname=studentname
-##        self.studentmark=studentmark
-##
-##    def display(self):
-##        print("student name",self.studentname,"student mark",self.studentmark)
-##
-##student1=student("jawahar",95)
-##student1.display()
-##student2=student("arun",85)
-##student2.display()
+class student:
+    def __init__(self,studentname,studentmark):
+        self.studentname=studentname
+        self.studentmark=studentmark
 
+    def display(self):
+        print("student name",self.studentname,"student mark",self.studentmark)
 
-
-
-
-#5.INHERITANCE PROGRAM:
-
-
-##class myclass1:
-##    def initialize (self):
-##        self.a=200
-##        self.b=50
-##
-##    def addition(self):
-##        self.c=self.a+self.b
-##        print("addition result is",self.c)
-##
-##class myclass2(myclass1):
-##    def subtraction(self):
-##        self.c=self.a-self.b
-##        print("subtraction result is",self.c)
-##
-##m1=myclass1()
-##m1.initialize()
-##m1.addition()
-##
-##m2=myclass2()
-##m2.initialize()
-##m2.subtraction()
+student1=student("jawahar",95)
+student1.display()
+student2=student("arun",85)
+student2.display()
 
 
 
-#6. LIST PROGRAM:
-
-##numbers=[100,200,300,400,500]
-##
-##for n in numbers:
-##    print(n)
 
 
+5.INHERITANCE PROGRAM:
 
-#7.TUPLE PROGRAM:
+
+class myclass1:
+    def initialize (self):
+        self.a=200
+        self.b=50
+
+    def addition(self):
+        self.c=self.a+self.b
+        print("addition result is",self.c)
+
+class myclass2(myclass1):
+    def subtraction(self):
+        self.c=self.a-self.b
+        print("subtraction result is",self.c)
+
+m1=myclass1()
+m1.initialize()
+m1.addition()
+
+m2=myclass2()
+m2.initialize()
+m2.subtraction()
+
+
+
+6. LIST PROGRAM:
+
+numbers=[100,200,300,400,500]
+
+for n in numbers:
+    print(n)
+
+
+
+7.TUPLE PROGRAM:
 
  #find average of student marks
 
---
+++ Day15-Assignment.py
@@ -0,0 +1,199 @@
+
+
+
+
+#1.IF ELSE PROGRAM:
+
+    #Find odd or even:
+
+##num=int(input("Enter the number"))
+##
+##
+##if num % 2==0:
+##        print("Even Number")
+##else:
+##    print("Odd Number")
+
+
+
+
+#2.MATCH CASE PROGRAM:
+
+  # Find bike:
+
+##bikes=input("Enter your bike name")
+##
+##match bikes:
+##
+##    case "kawasaki" | "ninja h2r" | "ducati" | "bmw m1000rr":
+##
+##        print("super bike")
+##
+##    case _:
+##        print("Normal bike")
+
+
+
+
+
+#3.FOR LOOP PROGRAM:
+
+   # Print numbers 1 to 10:
+
+##i = 1
+##
+##while i <= 10:
+##
+##    print(i)
+##
+##    i = i + 1
+
+
+    # Print even numbers 1 to 20:
+
+##i = 2
+##
+##while i <= 20:
+##    
+##    print (i)
+##
+##    i=i + 2
+
+
+
+
+
+
+
+#4. FOR LOOP PROGRAM:
+
+##numbers  = [ 10,20,30,40,50 ]
+##
+##for n in numbers:
+##
+##    print(n)
+    
+
+
+    #find  even numbers in list:
+
+##numbers = [ 1,2,3,4,5,6,7,8,9,10 ]
+##
+##for n in numbers:
+##    if n % 2 == 0:
+##        print(n)
+
+
+
+    #find odd numbers in list:
+
+##numbers = [ 1,2,3,4,5,6,7,8,9,10]
+##
+##for n in numbers:
+##    if n % 2 != 0:
+##        print(n)
+
+
+
+
+
+
+
+
+
+#5. LIST DATATYPE
+
+
+##bikes = ["kawasaki","ninja","bmw","ducati"]
+##
+##for bike in bikes:
+##    print("super bike: ",bike)
+
+
+
+
+
+#6. WITH PARAMETER AND WITH RETURN TYPE
+
+
+def addition(a,b):
+    c=a+b
+    return c
+a=100
+b=300
+
+result = addition(a,b)
+print(result)
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+   
+
+  
+
+
+
+
+
+
+
+
+        
+        
+
+

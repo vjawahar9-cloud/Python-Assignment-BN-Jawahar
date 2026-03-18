4eb625602e49333ac818b750fb3f4d675aeccb4e
vjawahar9-cloud <vjawahar9@gmail.com>
2026-03-18 22:13:05 +0530

    hii

---
+++ Day18-Assignment.py
@@ -0,0 +1,260 @@
+# SIMPLE PROGRAM FOR FOLLOWING TOPIC
+
+
+
+
+#1.FUNCTION WITH PARA , WITH RETURN
+
+##def addition(a,b):
+##    c=a+b
+##    return c
+##a=100
+##b=200
+##
+##result=addition(a,b)
+##
+##print("addition result is:",result)
+##
+##
+##
+##
+##def substraction(a,b):
+##    c=a-b
+##    return c
+##a=500
+##b=200
+##
+##result=substraction(a,b)
+##
+##print("substraction result is:",result)
+##
+##
+##
+##def multiplication(a,b):
+##    c=a*b
+##    return c
+##a=10
+##b=30
+##
+##result=multiplication(a,b)
+##
+##print("multiplication result is:",result)
+##
+##
+##
+##
+##
+##
+##
+###2.WHILE:
+##
+## #print 1 to 10
+##
+##i=1
+##while i <=10:
+##        print(i)
+##        i=i+1
+##
+##
+##
+##
+## #print even numbers 1 to 10
+##
+##i=2
+##while i <= 10:
+##    print(i)
+##    i=i+2
+##
+##
+##
+###print 10 to 1 reverse number
+##
+##
+##
+##
+##i=10
+##while i >=1:
+##    print(i)
+##    i=i-1
+##
+##
+##
+##
+##
+###3.FOR LOOP PROGRAM
+##
+##
+##    #print numbers 1 to 10
+##
+##numbers = [1,2,3,4,5,6,7,8,9,10]
+##
+##for n in numbers:
+##    print(n)
+##
+##
+##
+##
+##    #print odd numbers 1 to 20
+##
+##for i in range(1,21):
+##    if i % 2!= 0:
+##        print(i)
+##
+##
+##
+##    #print numbers 10 to 1 reverse
+##
+##for i in range(10,0,-1):
+##    print(i)
+##
+##
+##
+
+
+
+#4.LIST SET TUPLE DICTIONARY
+
+
+#LIST
+#Remove number 5 from the list.
+
+numbers=[5,10,15,20,25,30]
+
+for num in numbers:
+    if num !=5:
+        print(num)
+
+
+
+#TUPLE
+#Find the length of tuple.
+
+numbers = (5,10,15,20,25)
+
+numbers = len(numbers)
+
+print("length of tuple:",numbers)
+
+
+
+#SET
+#Add number 6 to the set
+
+numbers={1,2,3,4,5}
+
+numbers.add(6)
+
+print(numbers)
+
+
+#DICTIONARY
+#Create dictionary with name and age
+
+student={"name":"jawahar","age":24}
+print(student)
+
+
+
+
+
+
+
+#5.LAMBDA FUNCTION
+
+result = lambda a, b: a*b
+
+
+print( result(100, 100) )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

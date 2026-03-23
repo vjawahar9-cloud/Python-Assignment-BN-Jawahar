bcb3079bc5ddfdf75e3ca678177280b10c5c8030
vjawahar9-cloud <vjawahar9@gmail.com>
2026-03-23 14:30:48 +0530

    hii

---
+++ Day-20-Assignment.py
@@ -0,0 +1,23 @@
+#create a class and object with custom example
+
+
+class bike:
+
+    def  getdetails(self):
+        self.bikename=input("Enter the bike name")
+        self.bikeprice=int(input("Enter the bikeprice"))
+        self.bikecc=input("Enter the bikecc")
+
+    def displaydetails(self):
+        print("BIKE NAME : ",self.bikename)
+        print("BIKE PRICE : ",self.bikeprice)
+        print("BIKE CC : ",self.bikecc)
+
+b=bike()
+b.getdetails()
+b.displaydetails()
+
+
+#output
+                           
+
